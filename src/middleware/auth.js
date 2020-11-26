0950d343179ab705f8c2fee936feacd94fbce4fb
Admin <tranhoanggiang5@gmail.com>
2020-11-26 23:54:43 +0700

    Register - login - logout

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const auth = async(req, res, next) => {
+    let token = req.header('Authorization')
+    if (!token) {
+        res.status(401).send({ error: 'Bearer token is required' })
+    }
+
+    token = token.replace('Bearer ', '')
+
+    const data = jwt.verify(token, process.env.JWT_KEY)
+    try {
+        const user = await User.findOne({ _id: data._id, 'tokens.token': token })
+        if (!user) {
+            throw new Error()
+        }
+        req.user = user
+        req.token = token
+        next()
+    } catch (error) {
+        res.status(401).send({ error: 'Not authorized to access this resource' })
+    }
+
+}
+module.exports = auth
